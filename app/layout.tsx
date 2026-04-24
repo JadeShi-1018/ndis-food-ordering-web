1f5867bfe29a3ded169c77365bbed2ea2650bfc1
Jade <shiziqi022@gmail.com>
2026-04-25 05:45:40 +1000

    add environment variables

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import { AuthProvider } from "./context/AuthContext";
 
 
+
+
 import GlobalFooter from "@/components/GlobalFooter";
 import Navbar from "@/components/Navbar";
 import { ToastContainer } from "react-toastify";
