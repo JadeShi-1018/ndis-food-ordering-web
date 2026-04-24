d6c7281aeaee2924b04a53b46770d4bc55c594d6
Jade <shiziqi022@gmail.com>
2026-04-25 05:55:20 +1000

    add environment variables

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,11 +4,12 @@ import "./globals.css";
 import { AuthProvider } from "./context/AuthContext";
 
 
-
-
 import GlobalFooter from "@/components/GlobalFooter";
 import Navbar from "@/components/Navbar";
 import { ToastContainer } from "react-toastify";
+
+
+
 
 
 const geistSans = Geist({
